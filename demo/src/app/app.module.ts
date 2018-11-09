84427423cbf94ac6724d9f00afd13e3bccb09a47
Osei Fortune <fortune.osei@yahoo.com>
2018-11-08 22:56:13 -0400

    fix(demo): update

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy, Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import '@teamhive/capacitor-sentry';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { Plugins } from '@capacitor/core';
+const { Sentry } = Plugins;
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -20,4 +21,18 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule implements OnInit{
+  constructor(private platform: Platform) {}
+
+  ngOnInit(): void {
+    if (this.platform.is('ios')) {
+      Sentry.initialize({
+        dsn: 'https://0ffc67313d26438c9f036d4254e9ddb7@sentry.io/1319171'
+      });
+    } else if (this.platform.is('android')) {
+      Sentry.initialize({
+        dsn: 'https://518bc90005ec4760895b7253210e8315@sentry.io/1319173'
+      });
+    }
+  }
+}
--- demo/src/app/home/home.page.ts
+++ demo/src/app/home/home.page.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
-
+import { Plugins } from '@capacitor/core';
+import * as sentry from '@teamhive/capacitor-sentry';
+const { Sentry } = Plugins;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  crash(event) {
+    Sentry.crash();
+  }
+  setUser(event) {
+    Sentry.setUser({
+      email: 'fortune.osei@yahoo.com',
+      id: '1234',
+      data: {
+        name: 'Osei Forune'
+      }
+    });
+  }
 
+  setTags(event) {
+    Sentry.setTags({
+      tags: {
+        job: 'Dev'
+      }
+    });
+  }
+
+  setExtra() {
+    Sentry.setExtra({
+      extra: {
+        framework: 'Capacitor!!!'
+      }
+    });
+  }
+  clearContext(event) {
+    Sentry.clearContext();
+  }
+
+  captureMessage(event) {
+    Sentry.captureMessage({ message: 'Testing Capture Message' });
+  }
+
+  captureException(event) {
+    Sentry.captureException({
+      exception: sentry.SentryException.fromError(new Error('Testing Capture Execption'))
+    });
+  }
+
+  captureBreadcrumb(event) {
+    Sentry.captureBreadcrumb({
+      level: sentry.SentryLevel.INFO,
+      category: 'IDK',
+      data: {
+        stuff: '?'
+      },
+      message: 'Testing',
+      timeStamp: new Date().toISOString()
+    });
+  }
 }
