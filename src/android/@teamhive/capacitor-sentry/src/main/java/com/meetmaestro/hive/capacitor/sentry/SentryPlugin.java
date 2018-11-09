89d65d2b95c2d315ed8671e2a04993ad09f210ac
Osei Fortune <fortune.osei@yahoo.com>
2018-11-08 22:57:01 -0400

    feat(android): update plugin methods

--- src/android/@teamhive/capacitor-sentry/src/main/java/com/meetmaestro/hive/capacitor/sentry/SentryPlugin.java
+++ src/android/@teamhive/capacitor-sentry/src/main/java/com/meetmaestro/hive/capacitor/sentry/SentryPlugin.java
@@ -1,6 +1,7 @@
 package com.meetmaestro.hive.capacitor.sentry;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
@@ -8,16 +9,18 @@ import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.TimeZone;
 
 import io.sentry.Sentry;
 import io.sentry.android.AndroidSentryClientFactory;
 import io.sentry.event.Breadcrumb;
 import io.sentry.event.BreadcrumbBuilder;
-import io.sentry.event.Event;
 import io.sentry.event.EventBuilder;
-import io.sentry.event.User;
 import io.sentry.event.UserBuilder;
 
 @NativePlugin()
@@ -57,17 +60,17 @@ public class SentryPlugin extends Plugin {
         if (ip != null) {
             builder.setIpAddress(ip);
         }
-        if(username != null){
+        if (username != null) {
             builder.setUsername(username);
         }
-        HashMap<String,Object> dataMap = new HashMap<String, Object>();
+        HashMap<String, Object> dataMap = new HashMap<String, Object>();
         Iterator<String> keys = data.keys();
-        if(keys.hasNext()){
+        if (keys.hasNext()) {
             String key = keys.next();
             Object item = data.opt(key);
-            dataMap.put(key,item);
+            dataMap.put(key, item);
         }
-        if(!dataMap.isEmpty()){
+        if (!dataMap.isEmpty()) {
             builder.setData(dataMap);
         }
 
@@ -80,10 +83,10 @@ public class SentryPlugin extends Plugin {
     public void setTags(PluginCall call) {
         JSObject tags = call.getObject("tags");
         Iterator<String> keys = tags.keys();
-        if(keys.hasNext()){
+        if (keys.hasNext()) {
             String key = keys.next();
             String item = tags.getString(key);
-            Sentry.getContext().addTag(key,item);
+            Sentry.getContext().addTag(key, item);
         }
         call.resolve();
     }
@@ -92,16 +95,18 @@ public class SentryPlugin extends Plugin {
     public void setExtra(PluginCall call) {
         JSObject tags = call.getObject("extra");
         Iterator<String> keys = tags.keys();
-        if(keys.hasNext()){
+        if (keys.hasNext()) {
             String key = keys.next();
             Object item = tags.opt(key);
-            Sentry.getContext().addExtra(key,item);
+            Sentry.getContext().addExtra(key, item);
         }
+        call.resolve();
     }
 
     @PluginMethod()
     public void clearContext(PluginCall call) {
         Sentry.clearContext();
+        call.resolve();
     }
 
     @PluginMethod()
@@ -115,14 +120,74 @@ public class SentryPlugin extends Plugin {
 
     @PluginMethod()
     public void captureException(PluginCall call) {
-        Object exception = call.getData().opt("exception");
-        Sentry.capture(new Exception(exception.toString()));
+        JSObject exception = call.getObject("exception");
+        Exception e = new Exception(exception.getString("message"), new Throwable(exception.getString("stack")));
+        Sentry.capture(e);
+        call.resolve();
+    }
+
+
+    private Date fromISO8601UTC(String date) throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        TimeZone tz = TimeZone.getTimeZone("UTC");
+        dateFormat.setTimeZone(tz);
+        return dateFormat.parse(date);
     }
 
     @PluginMethod()
     public void captureBreadcrumb(PluginCall call) {
         BreadcrumbBuilder builder = new BreadcrumbBuilder();
+        String level = call.getString("level", "debug");
+        String category = call.getString("category", "");
+        String message = call.getString("message", "");
+        String timeStamp = call.getString("timeStamp", "");
+        JSObject data = call.getObject("data");
+        switch (level) {
+            case "info":
+                builder.setLevel(Breadcrumb.Level.INFO);
+                break;
+            case "warning":
+                builder.setLevel(Breadcrumb.Level.WARNING);
+                break;
+            case "error":
+                builder.setLevel(Breadcrumb.Level.ERROR);
+                break;
+            case "critical":
+                builder.setLevel(Breadcrumb.Level.CRITICAL);
+                break;
+            default:
+                builder.setLevel(Breadcrumb.Level.DEBUG);
+        }
+
+        if (data != null) {
+            HashMap<String, String> dataMap = new HashMap<>();
+            Iterator<String> keys = data.keys();
+            if (keys.hasNext()) {
+                String key = keys.next();
+                String item = data.getString(key);
+                dataMap.put(key, item);
+            }
+            builder.setData(dataMap);
+        }
+
+        if (!category.isEmpty()) {
+            builder.setCategory(category);
+        }
+
+        if (!message.isEmpty()) {
+            builder.setMessage(message);
+        }
+
+        if (!timeStamp.isEmpty()) {
+            try {
+                builder.setTimestamp(fromISO8601UTC(timeStamp));
+            } catch (ParseException e) {
+                call.error(e.getMessage(), e);
+                return;
+            }
+        }
         Sentry.getContext().recordBreadcrumb(builder.build());
+        call.resolve();
     }
 
 }
